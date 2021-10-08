be7726bbcfd2a551bbff4c019a63efba59127a29
weiyeeg <weiyeeg@gmail.com>
2021-10-07 18:22:23 -0700

    Added CSV Read of file arguments. Also added checks for arg1 and arg2 are existing files

--- find_discrepancy.rb
+++ find_discrepancy.rb
@@ -1,4 +1,26 @@
-def find_discrepancy(a,b)
-  puts 1
+require "csv"
+
+def find_discrepancy(file1, file2, concern = nil)
+  
+
+  # Check arg1 is an existing file
+  if (File.exist?(file1))
+    f1 = CSV.read(file1)
+  else
+    raise Exception.new "File does not exist"
+  end
+
+  # Check arg2 is an existing file
+  if (File.exist?(file2))
+    f2 = CSV.read(file2)
+  else
+    raise Exception.new "File does not exist"
+  end
+
+  
+
+
+  result = []
+  return result
 end
 
--- spec/test_parameter_spec.rb
+++ spec/test_parameter_spec.rb
@@ -7,6 +7,24 @@ RSpec.describe "Test Parameter Count" do
   end
 
   it 'Call find_discrepancy with 1 parameters' do
-    expect{find_discrepancy(1)}.to raise_error(ArgumentError)
+    expect{find_discrepancy("testfile.csv")}.to raise_error(ArgumentError)
   end
+
+end
+
+
+RSpec.describe "Test Parameter Type" do 
+
+  it 'Call with one incorrect file input as arg1'  do
+    expect{find_discrepancy('1', 'testfile.csv', 3)}.to raise_error("File does not exist")
+  end
+
+  it 'Call with one incorrect file input as arg2'  do
+    expect{find_discrepancy('testfile.csv', '1', 3)}.to raise_error("File does not exist")
+  end
+
+  it 'Call with existing files for arg1 and arg2'  do
+    expect(find_discrepancy("testfile.csv", "testfile.csv", 3)).to be_kind_of(Array)
+  end
+  
 end
